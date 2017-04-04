1e6f021cde814364a8f22198139f4e83338d5d37
puchk <kpuchalski@gmail.com>
2017-04-04 04:13:13 -0700

    Added gitignore, js files, txt file

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '2P2AzfYaDJLjeJpEhcqUctvCv',
+  consumer_secret: 'Hr51n6TNoal0yXnBRP7Gm5sDMcrWWnGWqlY6EXiPdfaGOwMG8b',
+  access_token_key: '849177960317833216-y8PTbCFPcdQxl0FPB9Txd5tupPAWUXY',
+  access_token_secret: 'kG5USGsBvr4xrAsK2wcXSGZ8X1WF8D1c68heZsjF2J8tI'
+}
