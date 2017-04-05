7bcf98934c631c180bceba6aafb0adb647746e39
puchk <kpuchalski@gmail.com>
2017-04-05 03:59:14 -0700

    Updated functions for twitter and spotify

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 var userCommand = process.argv[2];
-// console.log(userCommand);
+var additionalCommands = process.argv[3];
+
 
 // =======================TWITTER=======================
 var twitter = require('twitter');
@@ -20,21 +21,72 @@ function retrieveTweets(numberOfTweets){
 	});
 }
 // ----------------------POST TWEETS-----------------------
-/*client.post('statuses/update', {status: userCommand}, function(error, tweet, response) {
-  if (!error) {
-    console.log(tweet);
-  }
-});*/
+function postTweets(){
+		client.post('statuses/update', {status: additionalCommands}, function(error, tweet, response) {
+		  if (!error) {
+		    console.log(tweet);
+		  }
+		});
+}
+// =============================SPOTIFY================================
+var spotify = require('spotify');
+
+function searchSong(song){
+	spotify.search({ type: 'track', query: song }, function(err, data) {
+	  if ( err ) {
+	      console.log('Error occurred: ' + err);
+	  }
+
+	  var dataObject = data.tracks.items;
+	  // console.log(dataObject);
+
+	  for (var i=0; i<dataObject.length; i++) {
+	  // for (var i=0; i<1; i++) {
+		  songInfo(dataObject[i]);
+		};
+	});
+}
+
+function defaultSong(){
+	spotify.lookup({ type: 'track', id: "0hrBpAOgrt8RXigk83LLNE" }, function(err, data) {
+	  if ( err ) {
+	      console.log('Error occurred: ' + err);
+	  } 
+	  songInfo(data);
+	});
+}
+
+function songInfo(data){
+  // ARTIST
+  console.log("Artist: " + data.artists[0].name);
+  // SONG NAME
+  console.log("Song: " + data.name);
+  // PREVIEW LINK FROM SPOTIFY
+  console.log("Preview link: " + data.preview_url);
+  // ALBUM OF THE SONG
+  console.log("Album: " + data.album.name);
+  console.log("---------------------------------");
+}
 
 
-
-switch(userCommand){
+switch(userCommand) {
 	case "my-tweets":
-		retrieveTweets(3);
+		retrieveTweets(20);
 		break;
 	case "post-tweet":
-		console.log("post-tweet");
+		postTweets();
+		break;
+	case "spotify-this-song":
+		if (additionalCommands === undefined) {
+			defaultSong();
+			break;
+		} 
+		else {
+			searchSong(additionalCommands);
+			break;
+		}
+		searchSong(additionalCommands);
 		break;
 	default:
-		console.log("Command not recognized.")
+		console.log("Command not recognized.");
 }
